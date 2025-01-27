a95d22a1812972ee2474dffdf658fe58dc5fea98
adnan4k <fayomuhe5@gmail.com>
2025-01-26 20:09:19 -0800

    sidebar completed

--- storage/framework/views/db1d98d04e5bc8c0e7c802c0bf4ac722.php
+++ storage/framework/views/db1d98d04e5bc8c0e7c802c0bf4ac722.php
@@ -1,4 +1,4 @@
-<aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3 z-10 " id="sidenav-main">
+<aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3 z-0" id="sidenav-main">
 
 
     <div id="sidebar-container" class="">
