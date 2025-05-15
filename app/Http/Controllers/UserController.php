ae4dda62be82bc77b654f5d85d95f33b5340d925
adnan4k <fayomuhe5@gmail.com>
2025-05-15 18:27:46 +0300

    unique email

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -26,7 +26,7 @@ class UserController extends Controller
         // Validate the incoming request data
         $validatedData = $request->validate([
             'name'          => 'required|string|max:255',
-            'email'         => 'nullable|unique|email',
+            'email' => 'nullable|email|unique:users,email',
             // 'password'      => 'required',
             'phone_number'  => 'required|string|max:255',
             'referral_code' => 'nullable',
